6402b6c9d5eb3a99ab34aa6cb974f66263bfa307
CuteSpiderExe <101565618+CuteSpiderExe@users.noreply.github.com>
2025-01-14 20:42:12 +0300

    Add files via upload

---
+++ while/while.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+
+//С помощью цикла while разработать программу, которая будет 
+// вычислять сумму чисел нацело делящихся на 5. Цикл задать от 0 до введенного с клавиатуры числа.
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    int last_number, number, total;
+
+    cout << "Введите последнее число последовательности: ";
+    cin >> last_number;
+
+    number = 0;
+    total = 0;
+
+    while (number != last_number + 1) {
+        if (!(number % 5)) {
+            total += number;
+        }
+        number++;
+    }
+
+    cout << "Сумма чисел делящихся на 5 из последовательности [0, " << last_number << "] равна " << total << endl;
+
+    return 0;
+}
